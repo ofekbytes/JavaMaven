7cb5dd48d3b73ab383b2e63174d8203087cacaf8
Yaron Kessler <Yaron.Kessler@brinks.com>
2018-05-11 13:32:46 +0300

    interface test

--- src/main/java/dataBase/sqlite/InterfaceUserList.java
+++ src/main/java/dataBase/sqlite/InterfaceUserList.java
@@ -4,4 +4,10 @@ public interface InterfaceUserList
 {
 	public boolean fnCreateDataBase (String DataBaseName);
 	public boolean fnCreateTable (String DataBaseName, String TabelName);
+	
+	
+	
+	public boolean fnEraseAllRecord (String DataBaseName, boolean blAreYouSure);
+	public boolean fnEraseDatabaseFile(String DataBaseName, boolean blAreYouSure);
+	
 }
