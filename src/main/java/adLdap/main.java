75f9822a4fdd0a053bd827590656fb07bbb4d22e
Yaron Kessler <Yaron.Kessler@brinks.com>
2018-05-14 16:50:02 +0300

    ldap test

---
+++ src/main/java/adLdap/main.java
@@ -0,0 +1,33 @@
+package adLdap;
+
+import javax.naming.NamingException;
+
+public class main {
+
+//	public main() {
+//	}
+
+	public static void main(String[] args) 
+	{
+
+		System.out.println("\n***Method #1: \n");
+		OutsideDomain o = new OutsideDomain();
+
+		
+		
+//		OutsideDomainMemberOf omf = null;
+//		
+//		try 
+//		{
+//			System.out.println("\n***Method #2: \n");
+//			omf = new OutsideDomainMemberOf();
+//		}
+//		catch (NamingException e) 
+//		{
+//			
+//			e.printStackTrace();
+//		}
+		
+	}
+
+}
