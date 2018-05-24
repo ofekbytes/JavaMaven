932bf11829252ea5fd436c3a4ba80c982e76c7ba
Yaron Kessler <Yaron.Kessler@brinks.com>
2018-05-24 16:25:25 +0300

    util.logging.*

---
+++ src/main/java/logApi/Log4j.java
@@ -0,0 +1,14 @@
+package logApi;
+
+/****
+ * 
+ *
+ ****/
+
+public class Log4j {
+
+	public Log4j() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/main/java/logApi/LogUtil.java
@@ -0,0 +1,33 @@
+
+/***
+ * LogUtil - java log util
+ * API: java.util.logging.*;
+ */
+
+
+package logApi;
+
+import java.util.logging.Level;
+//import java.util.logging.*;
+import java.util.logging.Logger;
+
+public class LogUtil 
+{
+
+	
+	private final static Logger logIt = Logger.getLogger(LogUtil.class.getName());
+	
+	public LogUtil() 
+	{
+		logIt.setLevel(Level.INFO); //level.*
+	}
+
+	
+	public static void main(String[] args) 
+	{
+		LogUtil la = new LogUtil();
+		
+
+	}
+
+}
