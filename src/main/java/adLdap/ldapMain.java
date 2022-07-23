866c9b41592febe97f39a7366c561615da88411d
yaron <yaron.kessler41@gmail.com>
2022-07-23 10:44:30 +0300

    test

--- src/main/java/adLdap/ldapMain.java
+++ src/main/java/adLdap/ldapMain.java
@@ -15,25 +15,26 @@ public class ldapMain {
 		/***
 		 * User Groups in Active Directory / LDAP (with debug System.out/System.err)
 		 */
-		System.out.println("\n***Method #1: \n");
-		OutsideDomain o = new OutsideDomain();
+// OK//		
+//		System.out.println("\n***Method #1: \n");
+//		OutsideDomain o = new OutsideDomain();
 
 		
 /***
  * **2Fix**		
  */
-//		OutsideDomainMemberOf omf = null;
-//		
-//		try 
-//		{
-//			System.out.println("\n***Method #2: \n");
-//			omf = new OutsideDomainMemberOf();
-//		}
-//		catch (NamingException e) 
-//		{
-//			
-//			e.printStackTrace();
-//		}
+		OutsideDomainMemberOf omf = null;
+		
+		try 
+		{
+			System.out.println("\n***Method #2: \n");
+			omf = new OutsideDomainMemberOf();
+		}
+		catch (NamingException e) 
+		{
+			
+			e.printStackTrace();
+		}
 		
 	}
 
